e8d8aa0492e54684fc8ae9f2b9016d756480268b
Avery Nguyen <averynguyen@Averys-MacBook-Air.local>
2020-07-06 20:20:43 -0600

    added show, confirm, and empty stories

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,10 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment"
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
+import Confirm from 'components/Appointment/Confirm'
 
 
 storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
@@ -136,3 +140,18 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add('Appointment', () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm"/>)
+  .add("Empty", () => <Empty onAdd = {action("onAdd")} />)
+  .add("Show", () => 
+    <Show 
+    student="Lydia Miller-Jones"
+    interviewer={interviewers}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+    />)
+  .add("Confirm", () => 
+  <Confirm 
+    message="Delete the appointment?"
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
+  />)
