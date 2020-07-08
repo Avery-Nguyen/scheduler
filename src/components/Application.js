dbc12e440308aa7d90d8f01bb3b21b761961fcd2
Avery Nguyen <averynguyen@Averys-MacBook-Air.local>
2020-07-08 16:11:32 -0600

    added custom hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,45 +4,8 @@ import Appointment from "components/Appointment"
 import "components/Application.scss";
 import DayList from "components/DayList";
 import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { useVisualMode } from "hooks/useVisualMode"
 
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "2pm",
-//   },
-//   {
-//     id: 3,
-//     time: "3pm",
-//     interview: {
-//       student: "Bob Steve",
-//       interviewer: { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "4pm",
-//     interview: {
-//       student: "Avery Nguyen",
-//       interviewer: { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-//     }
-//   }
-// ];
 
 export default function Application(props) {
 
@@ -54,13 +17,7 @@ export default function Application(props) {
   });
 
   const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({ ...prev, days }));
 
-  // useEffect(() => {
-  //   axios.get(`/api/days`).then((response) => {
-  //     setDays(response.data)
-  //   });
-  // });
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'), 
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+export default function useVisualMode(initial) {
+  
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+
+  function transition (newMode, replace = false) {
+    if (replace) {
+      setMode(newMode);
+      return { transition };
+    }
+
+    setMode(newMode);
+    setHistory(prev => ([...prev, newMode]));
+    return { transition };
+  }
+
+  function back () {
+    if(history.length <= 1 ) {
+      setMode(initial);
+      setHistory(initial); 
+      return { back };
+    }
+    history.pop();
+    setMode(history[history.length-1]);
+    return { back };
+  }
+
+  return { mode, transition, back };
+
+}
